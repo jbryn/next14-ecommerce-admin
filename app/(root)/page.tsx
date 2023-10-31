54762103c3bc964782387d6d7b9325ee2619e251
Jan Bryński <brynski.jan@gmail.com>
2023-10-31 15:09:07 +0100

    setup: add clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="p-4">
+      <Button size="lg" variant="default">
+        Click
+      </Button>
+    </div>
+  );
 }
