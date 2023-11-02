def065be79501264f7d13e5aad68f7e4aa237ed6
Jan Bryński <brynski.jan@gmail.com>
2023-11-02 20:03:43 +0100

    feat: modals provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="p-4">
-      <Modal title="test" description="test" onClose={() => {}} isOpen>
-        children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 }
